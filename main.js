4a93a12ab59729c6ac6eb95ef89919605101de41
Liran Zuk <jozuk97@hotmail.co.il>
2023-07-11 19:51:34 +0300

    Add messages to each slide

--- main.js
+++ main.js
@@ -55,16 +55,20 @@ d3.csv('ds_salaries.csv').then(data => {
     updateButtons();
     createChart(data.filter(row => +row.work_year === GetCurrentYear()), remote_ratio_selections);
 
-    title.text(`${GetCurrentYear()} Salary Trends`);
+    title.text(`Data Science Salary Trends in ${GetCurrentYear()}`);
 
     if (GetCurrentYear() === 2020) {
-      message.text("Message for 2020 Message Message  Message  Message  Message  Message  Message  Message  Message  Message  Message");
+      message.text(
+        "In 2020, with a diversity of 23 job titles, the average salary stood at $92.3K with a considerable standard deviation of $82.3K. The year was marked by a high average remote ratio of 65%, reflecting the swift shift to remote work due to the COVID-19 pandemic.");
     } else if (GetCurrentYear() === 2021) {
-      message.text("Message for 2021 Message Message  Message  Message  Message  Message  Message  Message  Message  Message  Message");
+      message.text(
+        "2021 saw a significant expansion in the job market, with the emergence of 45 different job titles. The average salary rose slightly to $94K, while the standard deviation in salary reduced to $68.6K. The average remote ratio remained high at 68%, as the effects of the pandemic persisted.");
     } else if (GetCurrentYear() === 2022) {
-      message.text("Message for 2022 Message Message  Message  Message  Message  Message  Message  Message  Message  Message  Message");
+      message.text(
+        "By 2022, there was an explosive growth in the data science field, with 67 job titles and a substantial increase in the average salary to $133K. The standard deviation in salary shrunk further to $58.9K, indicating a consolidation of the pay scale. Interestingly, the average remote ratio dipped to 55%, a possible indication of the easing of pandemic restrictions.");
     } else if (GetCurrentYear() === 2023) {
-      message.text("Message for 2023 Message Message  Message  Message  Message  Message  Message  Message  Message  Message  Message");
+      message.text(
+        "In 2023, the variety of job titles plateaued at 65, but the average salary continued its upward trend to reach $149K. The standard deviation in salary showed a slight increase to $61.3K. The most notable shift was the significant drop in the average remote ratio to 34%, suggesting a major shift back to in-office work as the world adapted to the post-pandemic era.");
     }
   }
 });
